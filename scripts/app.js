5d55b667e5049be24e590f291673ceae0062d5ca
webjay <jacob@saxberg.dk>
2013-01-28 10:03:34 +0100

    GA tuned, fixed link to buy

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 jQuery(function($){
-	
+
+	// set affix for book menu
 	$('#bookindex ol').affix({
 		offset: {
 			top: function () {
@@ -9,6 +10,23 @@ jQuery(function($){
 			bottom: function () {
 				return $('#sources').height() + $('#natasha').height();
 			}
+		}
+	});
+
+	// track outbound links
+	$.expr[':'].external = function (obj) {
+		return (obj.hostname != location.hostname);
+	};
+	$('a:external').on('click', function (event) {
+		event.preventDefault();
+		var url = $(this).attr('href');
+		try { 
+			_gaq.push(['_trackEvent', 'Outbound Links', url]);
+			_gaq.push(function(){
+				document.location.href = url;
+			});
+		} catch (err) {
+			document.location.href = url;
 		}
 	});
 	
