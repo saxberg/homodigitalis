8546ab1572fe2db983feaa27eaee7b5d57cf5dd1
webjay <jacob@saxberg.dk>
2013-01-29 00:41:27 +0100

    deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,21 +55,25 @@ module.exports = function (grunt) {
 	grunt.registerTask('default', ['uglify', 'recess', 'template']);
 
 	// gitploy
-	function run (cmd, callback) {
-		grunt.util.spawn({ cmd: cmd }, function (err) {
-			if (err !== null) {
-				return grunt.util.error(err);
-			}
-			callback();
-		});
-	}
-	grunt.registerTask('deploy', 'Deploy to GitHub gh-pages', function(){
+	grunt.registerTask('gitploy', 'Deploy to GitHub gh-pages', function(){
+		var exec = require('child_process').exec;
+		var run = function (cmd, callback) {
+			return exec(cmd, function(err, stdout, stderr) {
+				if (err !== null) {
+					throw err;
+				}
+				console.log(stdout + stderr);
+				return typeof callback === "function" ? callback() : void 0;
+			});
+		};
+		var done = this.async();
 		var paths = ['img', 'lib', 'media', '404.html', 'BingSiteAuth.xml', 'CNAME', 'favicon.ico', 'google001da78e150395d4.html', 'index.html', 'kilder.html', 'robots.txt', 'sitemap.xml']
 		run('git checkout gh-pages', function(){
 			run('git checkout master -- ' + paths.join(' '), function(){
 				run('git commit -am "updates from master"', function(){
 					run('git push origin gh-pages', function(){
 						run('git checkout master', function(){
+							done();
 						});
 					});
 				});
