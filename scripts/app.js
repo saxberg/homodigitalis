6d0d9291cfad5c975bd874f3b18ad3ce5ecc08ab
webjay <jacob@saxberg.dk>
2013-02-01 15:26:19 +0100

    fb async

--- scripts/app.js
+++ scripts/app.js
@@ -76,5 +76,12 @@ jQuery(function($){
 		});
 
 	};
-	
+
+	(function() {
+		var e = document.createElement('script'); e.async = true;
+		e.src = document.location.protocol +
+		'//connect.facebook.net/en_US/all.js';
+		document.getElementById('fb-root').appendChild(e);
+	}());	
+
 });
