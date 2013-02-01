f1d6ff5db03ca54da7515bf2ff205c50172e5abd
webjay <jacob@saxberg.dk>
2013-02-01 15:03:28 +0100

    forgot fbchannel.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
 			});
 		};
 		var done = this.async();
-		var paths = ['img', 'lib', 'media', '404.html', 'BingSiteAuth.xml', 'CNAME', 'favicon.ico', 'google001da78e150395d4.html', 'index.html', 'kilder.html', 'robots.txt', 'sitemap.xml']
+		var paths = ['img', 'lib', 'media', '404.html', 'BingSiteAuth.xml', 'CNAME', 'favicon.ico', 'google001da78e150395d4.html', 'index.html', 'kilder.html', 'robots.txt', 'sitemap.xml', 'fbchannel.html']
 		run('git checkout gh-pages', function(){
 			run('git checkout master -- ' + paths.join(' '), function(){
 				run('git commit -am "updates from master"', function(){
